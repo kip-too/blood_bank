da27ec36c90c1cd99810eb23a59aabea1e68014e
kip-too <hoseakiptoo201@gmail.com>
2023-03-26 08:20:31 +0300

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
-import 'dart:html';
-
-import 'package:blood_bank/keys/keys.dart';
 import 'package:blood_bank/repositories/authrepo/auth_repo.dart';
 import 'package:blood_bank/screens/authentication/phone_sign_up.dart';
 import 'package:blood_bank/screens/request/create_request.dart';
 import 'package:blood_bank/screens/searchdonor/search_donor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'blocs/auth/authentication_bloc.dart';
 import 'firebase_options.dart';
 import 'screens/authentication/Signup_screen.dart';
 import 'screens/authentication/otp_page.dart';
 import 'screens/bloodbank/blood_bank.dart';
 import 'screens/exports_screens.dart';
 
-Future<void> main()  async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MainApp());
-
+  await Firebase.initializeApp(
+      /*options: DefaultFirebaseOptions.currentPlatform*/);
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -32,8 +28,9 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: PhoneSignUp.id,
       routes: {
-        PhoneSignUp.id:(context) => const PhoneSignUp(),
+        PhoneSignUp.id: (context) => const PhoneSignUp(),
         SignUpPage.id: (context) => const SignUpPage(),
+        OTPPage.id: (context) => const OTPPage(),
         HomeScreen.id: (context) => const HomeScreen(),
         SearchDonorScreen.id: (context) => const SearchDonorScreen(),
         CreateRequestPage.id: (context) => const CreateRequestPage(),
